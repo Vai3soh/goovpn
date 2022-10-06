f06a98613052b4d1f4664254fd383bef007c7431
Vai3soh <work.rwx.seven@gmail.com>
2022-10-06 15:38:59 +1000

    Add new entry point for windows OS and add config

---
+++ cmd/app/main_win.go
@@ -0,0 +1,33 @@
+//go:build windows
+// +build windows
+
+package main
+
+import (
+	"flag"
+	"log"
+	"runtime"
+
+	"github.com/Vai3soh/goovpn/config"
+	"github.com/Vai3soh/goovpn/internal/app"
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+var Config = flag.String(
+	"config", "C:\\ProgramData\\config_win.yml",
+	"path to configuration file",
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	cfg := config.Config{}
+	err := cleanenv.ReadConfig(*Config, &cfg)
+	if err != nil {
+		log.Fatalf("Config error: [%s]\n", err)
+	}
+
+	app.Run(&cfg)
+}
